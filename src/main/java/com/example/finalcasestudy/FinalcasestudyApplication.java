ee934cd474633c47c3f74cb5aca6be951814041c
divyatsamadhiya <sdivyatuniverse@gmail.com>
2020-05-13 18:07:25 +0530

    project created

---
+++ src/main/java/com/example/finalcasestudy/FinalcasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalcasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalcasestudyApplication.class, args);
+	}
+
+}
